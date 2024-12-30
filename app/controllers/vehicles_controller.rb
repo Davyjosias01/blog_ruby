98c04ef3d91434627ba0d8848853fd937752d04c
davy <davy.josias@gmail.com>
2024-12-30 11:52:22 -0300

    comit de segunda 30-12-24

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -3,7 +3,31 @@ class VehiclesController < ApplicationController
       @vehicles = Vehicle.all.order(year: :desc) # para crescente é: ":asc"
   end
 
+
   def new
     @vehicles = Vehicle.new
+  end
+
+
+  def create
+    @vehicle = Vehicle.new(vehicle_params)
+
+    if @vehicle.save
+      redirect_to vehicle_path(@vehicle)
+    else
+      render :new
+    end
+  end
+
+
+  def show
+      @vehicle = Vehicle.find(params[:id])
+  end
+
+
+  private
+
+  def vehicle_params
+    params.require(:vehicle).permit(:brand, :model, :year, :kind, :plate)
   end
 end
